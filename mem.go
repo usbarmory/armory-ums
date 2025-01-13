65f52ff49c00421c2868907a125a3468cf89b356
Andrea Barisani <andrea@inversepath.com>
2025-01-13 18:43:08 +0100

    tidying

--- mem.go
+++ mem.go
@@ -14,8 +14,8 @@ import (
 	"github.com/usbarmory/tamago/dma"
 )
 
-// Override usbarmory pkg ramSize and `mem` allocation, as this application
-// requires large DMA descriptors.
+// Override standard memory allocation as this application requires large DMA
+// descriptors.
 
 //go:linkname ramSize runtime.ramSize
 var ramSize uint32 = 0x10000000 // 256MB
